2d4f0e65d8433523f366969cf56f110875a4d6e8
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-23 02:46:00 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,17 @@
-class Cat
-  # code goes here
-end
+class Cat 
+  attr_reader :name
+  attr_accessor :owner, :mood 
+  
+  @@cats = []
+  
+  def initialize(name, owner, mood = "nervous")
+    @name = name 
+    @owner = owner
+    @mood = mood
+    @@cats << self
+  end 
+  
+   def self.all
+    @@cats
+  end
+end 
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
-class Dog
-  # code goes here
-end
+class Dog 
+  attr_reader :name
+  attr_accessor :owner, :mood 
+  
+  @@dogs = []
+  
+  def initialize(name, owner, mood = "nervous")
+    @name = name 
+    @owner = owner
+    @mood = mood
+    @@dogs << self
+  end 
+  
+   def self.all
+    @@dogs
+  end
+end 
